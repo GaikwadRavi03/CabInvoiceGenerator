cd35979ce21af4ac61aa66cd506b0af4f4384f5d
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-12-17 10:44:19 +0530

    Added TestCases to calculte rides for particular user in multiple months.

--- src/test/java/com/cabinvoicegenerator/RideRepositoryTest.java
+++ src/test/java/com/cabinvoicegenerator/RideRepositoryTest.java
@@ -55,4 +55,42 @@ public class RideRepositoryTest {
         } catch (InvoiceServiceException e) {
         }
     }
+
+    @Test
+    public void givenMessage_WhenMultipleUsersAddedRides_ShouldReturnUsersInvoiceSummary() {
+        try {
+            RideRepository rideRepository = new RideRepository();
+            String userId1 = "a@b.com";
+            Rides[] rides1 = {new Rides(1.0, 10, TypesOfCabs.NORMAL_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.PREMIUM_RIDES)
+            };
+            Rides[] rides2 = {new Rides(3.0, 5, TypesOfCabs.NORMAL_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.PREMIUM_RIDES)
+            };
+            Rides[] rides3 = {new Rides(2.0, 5, TypesOfCabs.PREMIUM_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.NORMAL_RIDES)
+            };
+            String userId2 = "x@z.com";
+            Rides[] user2Rides1 = {new Rides(1.0, 10, TypesOfCabs.NORMAL_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.PREMIUM_RIDES)
+            };
+            Rides[] user2Rides2 = {new Rides(3.0, 5, TypesOfCabs.NORMAL_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.PREMIUM_RIDES)
+            };
+            Rides[] user2Rides3 = {new Rides(2.0, 5, TypesOfCabs.NORMAL_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.NORMAL_RIDES),
+                    new Rides(0.1, 1, TypesOfCabs.PREMIUM_RIDES)
+            };
+            rideRepository.addRides(userId1, rides1);
+            rideRepository.addRides(userId1, rides2);
+            rideRepository.addRides(userId1, rides3);
+            rideRepository.addRides(userId2, user2Rides1);
+            rideRepository.addRides(userId2, user2Rides2);
+            rideRepository.addRides(userId2, user2Rides3);
+
+            List<Rides> ridesList = rideRepository.getRides(userId2);
+            Assert.assertEquals(7, ridesList.size());
+        } catch (InvoiceServiceException e) {
+        }
+    }
 }
